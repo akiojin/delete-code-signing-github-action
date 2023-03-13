48b448258db36b1052f3ad95f1078a16d4edf341
Akio Jinsenji <akio-jinsenji@cloud-creative-studios.com>
2023-03-14 08:46:23 +0900

    Update

--- dist/index.js
+++ dist/index.js
@@ -4627,11 +4627,14 @@ async function Run() {
     try {
         const text = await keychain_1.Keychain.GetCodeSigning();
         // 2) DB1BD76E36121221A91216D4B69C767E998A4B69 "Apple Development: Akio Jinsenji (Y7S6CV6TA8)"
-        const regex = /\s*\d\)\s(?<Hash>\w*)\s"[Apple|iPhone]*\s(?<Type>.*):\s(?<Creator>.*)\s\((?<TeamID>\w*)\)"/g;
+        const regex = /\s*\d\)\s(?<Hash>\w*)\s"[Apple|iPhone]*\s(?<Type>.*):\s(?<Publisher>.*)\s\((?<IssuerID>\w*)\)"/g;
         const hashes = Array.from(text.matchAll(regex), match => match.groups)
-            .filter(match => match['Type'] === core.getInput('type'))
-            .filter(match => match['Creator'] === core.getInput('creater'))
-            .map(match => match['Hash']);
+            .filter(match => !!match['Type'] && match['Type'] === core.getInput('type'))
+            .filter(match => !!match['Publisher'] && match['Publisher'] === core.getInput('publisher'))
+            .map(match => {
+            console.log(match);
+            match['Hash'];
+        });
         const promises = hashes
             .map(hash => keychain_1.Keychain.DeleteCodeSigning(hash));
         await Promise.all(promises);
--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,15 @@ async function Run()
         const text = await Keychain.GetCodeSigning()
 
         // 2) DB1BD76E36121221A91216D4B69C767E998A4B69 "Apple Development: Akio Jinsenji (Y7S6CV6TA8)"
-        const regex = /\s*\d\)\s(?<Hash>\w*)\s"[Apple|iPhone]*\s(?<Type>.*):\s(?<Creator>.*)\s\((?<TeamID>\w*)\)"/g
+        const regex = /\s*\d\)\s(?<Hash>\w*)\s"[Apple|iPhone]*\s(?<Type>.*):\s(?<Publisher>.*)\s\((?<IssuerID>\w*)\)"/g
 
         const hashes = Array.from(text.matchAll(regex), match => match.groups!)
-            .filter(match => match['Type'] === core.getInput('type'))
-            .filter(match => match['Creator'] === core.getInput('creater'))
-            .map(match => match['Hash'])
+            .filter(match => !!match['Type'] && match['Type'] === core.getInput('type'))
+            .filter(match => !!match['Publisher'] && match['Publisher'] === core.getInput('publisher'))
+            .map(match => {
+                console.log(match)
+                match['Hash']
+            })
 
         const promises = hashes
             .map(hash => Keychain.DeleteCodeSigning(hash!))
